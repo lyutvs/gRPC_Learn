902210ab3006a2deacc82b42ad126fd33690e4f4
sehyoun <lyutvs@gmail.com>
2021-09-05 23:24:19 +0900

    ⚡️ :: GrpcApplication

--- grpc-server/src/main/java/com/example/grpc/GrpcApplication.java
+++ grpc-server/src/main/java/com/example/grpc/GrpcApplication.java
@@ -1,13 +1,29 @@
 package com.example.grpc;
 
+import com.example.grpc.server.GrpcServer;
+import com.example.grpc.server.GrpcServerService;
+import io.grpc.BindableService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class GrpcApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(GrpcApplication.class, args);
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        final int port = 54321;
+        final BindableService helloService = (BindableService) new GrpcServerService();
+
+        GrpcServer server = new GrpcServer(port, helloService);
+
+        server.start();
+
+        Runtime.getRuntime().addShutdownHook(
+                new Thread(() -> server.shutdown())
+        );
     }
+
 
 }
