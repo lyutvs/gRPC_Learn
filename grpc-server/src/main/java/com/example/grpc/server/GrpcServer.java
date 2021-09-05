d43ba003f056a8d6b8e38fd07bd77446dcce36fb
sehyoun <lyutvs@gmail.com>
2021-09-05 23:24:27 +0900

    ⚡️ :: GrpcServer

--- grpc-server/src/main/java/com/example/grpc/server/GrpcServer.java
+++ grpc-server/src/main/java/com/example/grpc/server/GrpcServer.java
@@ -1,2 +1,37 @@
-package com.example.grpc.server;public class GrpcServer {
+package com.example.grpc.server;
+
+
+import io.grpc.BindableService;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+public class GrpcServer {
+
+    private final Logger logger = Logger.getLogger(GrpcServer.class.getName());
+
+    private final int port;
+    private final Server server;
+
+    public GrpcServer(int port, BindableService service) throws IOException {
+        this.port = port;
+        this.server = ServerBuilder.forPort(port)
+                .addService(service)
+                .build();
+    }
+
+    public void start() throws IOException, InterruptedException {
+        this.server.start();
+        logger.info("gRPC Server Listening on port " + port);
+        this.server.awaitTermination();
+    }
+
+    public void shutdown() {
+        System.err.println("gRPC 서버 종료..");
+        server.shutdown();
+        System.err.println("gRPC 서버 종료 완료");
+    }
+
 }
