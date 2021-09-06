ad35dfca10cd099ebd3e0cb6def6987b04ae8b78
sehyoun <lyutvs@gmail.com>
2021-09-06 15:55:48 +0900

    ⚙️ :: Init Project

---
+++ grpc example/src/test/java/com/example/grpcexample/GrpcexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.grpcexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcexampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
