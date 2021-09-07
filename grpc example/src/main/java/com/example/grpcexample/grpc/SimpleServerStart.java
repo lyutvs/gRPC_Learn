72c65002a57b055b1f3b92147fd2dbfbc06c6e83
sehyoun <lyutvs@gmail.com>
2021-09-07 23:10:23 +0900

    ⚡️ :: SimpleServiceStart

--- grpc example/src/main/java/com/example/grpcexample/grpc/SimpleServerStart.java
+++ grpc example/src/main/java/com/example/grpcexample/grpc/SimpleServerStart.java
@@ -1,2 +1,42 @@
-package com.example.grpcexample.grpc;public class SimpleServerStart {
+package com.example.grpcexample.grpc;
+
+import io.grpc.Server;
+import io.grpc.netty.NettyServerBuilder;
+
+import java.util.concurrent.TimeUnit;
+
+public class SimpleServerStart {
+
+    private int PORT=8888;
+    private Server server;
+
+    private void start() throws Exception{
+        server = NettyServerBuilder.forPort(PORT).addService(new SimpleServer().bindService()).build();
+        server.start();
+
+        Runtime.getRuntime().addShutdownHook(new Thread(){
+            @Override
+            public void  run(){
+                System.err.println("*** shutting down gRPC server since JVM is shutting down");
+                SimpleServerStart.this.stop();
+                System.err.println("*** server shut down");
+            }
+        });
+    }
+
+    private void stop(){
+        try {
+            server.awaitTermination(2, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        final SimpleServerStart simpleServerStart = new SimpleServerStart();
+        simpleServerStart.start();
+        TimeUnit.SECONDS.sleep(3000);
+    }
+
+
 }
