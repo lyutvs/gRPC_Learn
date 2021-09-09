52bd18c06c764c689424b9ce00d1de675803bca0
sehyoun <lyutvs@gmail.com>
2021-09-09 19:14:05 +0900

    ⚙️ :: Init Project

---
+++ test-proto/src/test/java/com/example/testproto/TestProtoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testproto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestProtoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
